4f9c72fffb4b846ec64e9f00b2196e4879aefc40
Vladimir <vlpetrov02@gmail.com>
2023-01-18 20:59:00 +0200

    improved logic and added comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,46 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import DifficultySelect from './components/DifficultySelect';
-import questions from './assets/questions.json';
+import questions from './assets/questions.json'; // i have my data in a separate file
 
 function App() {
-  const [maxDifficulty, setMaxDifficulty] = useState(null);
-  const [currentQuestion, setCurrentQuestion] = useState(null);
-  const [displayAnswer, setDisplayAnswer] = useState(false);
+  const [maxDifficulty, setMaxDifficulty] = useState(null); // this will be an array with 2 items [minimum, maximum]
+  const [currentQuestion, setCurrentQuestion] = useState(null); // this will save the chosen question state
+  const [displayAnswer, setDisplayAnswer] = useState(false); // this determines if we should show the answer
 
+  // this choses a random question when maxDifficulty changes (becomes a value [] or null)
   useEffect(() => {
     pickQuestion();
   }, [maxDifficulty]);
 
   function pickQuestion() {
-    let availableQuestions = questions.filter(q => q.difficulty <= maxDifficulty);
+    // here we filter out the question by 2 conditions, difficulty more or equal (>=) than the minimum ([0]) and difficulty less or equal (<=) than the maximum ([1])
+    let availableQuestions = questions.filter(q => q.difficulty >= maxDifficulty?.[0] && q.difficulty <= maxDifficulty?.[1]);
+
+    // if we already have a question, exclude it from the available ones (to avoid picking the same question twice)
+    if (currentQuestion) availableQuestions = availableQuestions.filter(que => que.question !== currentQuestion.question);
+
+    // chose a random question from the available ones and set it as currentQuestion
     let chosenQuestion = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
     setCurrentQuestion(chosenQuestion);
   }
 
+  // notice what "hardReset" is doing and why we have a default value for it
   function resetQuestion(hardReset = false) {
     pickQuestion();
     setDisplayAnswer(false);
     if (hardReset) setMaxDifficulty(null);
   }
 
+  // this function returns a jsx element conditionally, take some time to follow the logic
   function controlButtons() {
     if (!maxDifficulty) return <DifficultySelect setMaxDifficulty={setMaxDifficulty} />;
 
     if (!displayAnswer) return <button onClick={() => setDisplayAnswer(true)}>Show answer</button>;
 
     if (maxDifficulty && displayAnswer)
+      // using fragments (<></>) is good when you don't really want a <div> but you need to return multiple elements
+      // btw you cant return multiple elements in 1 return, it don't like arrays
       return (
         <>
           <button onClick={() => resetQuestion()}>Next Question</button>
--- src/components/DifficultySelect.jsx
+++ src/components/DifficultySelect.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function DifficultySelect({ setMaxDifficulty }) {
   return (
     <div>
-      <button onClick={() => setMaxDifficulty(3)}>1 - 3</button>
-      <button onClick={() => setMaxDifficulty(6)}>4 - 6</button>
-      <button onClick={() => setMaxDifficulty(10)}>7 - 10</button>
+      <button onClick={() => setMaxDifficulty([1, 3])}>1 - 3</button>
+      <button onClick={() => setMaxDifficulty([4, 6])}>4 - 6</button>
+      <button onClick={() => setMaxDifficulty([7, 10])}>7 - 10</button>
     </div>
   );
 }
